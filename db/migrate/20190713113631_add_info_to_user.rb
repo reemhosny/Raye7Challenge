ce125ba66732f078ea8d0c9334195d0df0db59d5
reemhosny <reem.hosny@larienagowns.com>
2019-07-22 17:36:16 +0200

    fix

--- db/migrate/20190713113631_add_info_to_user.rb
+++ db/migrate/20190713113631_add_info_to_user.rb
@@ -1,8 +1,8 @@
 class AddInfoToUser < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :full_name, :string
-    add_column :users, :phone_number, :string
-    add_column :users, :role, :integer
+    # add_column :users, :full_name, :string
+    #add_column :users, :phone_number, :string
+    #add_column :users, :role, :integer
 
 
 
